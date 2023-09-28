4a922d71d6ef9273378333e4c93646b573556bb8
Paula Barroni <pbarroni95@gmail.com>
2023-09-27 23:04:29 -0300

    Preentrega uno - primer commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+import CartWidget from "./CartWidget";
+import M from "materialize-css";
+
+
+
+export default NavBar;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,34 @@
+
+
+/*function App() {
+  return "Hola mundo jiji"
+}
+
+*/
+
+const App = () => {
+
+  return (
+    <>
+
+    <header id="header">
+    <h1> The Rocy Lozy Fit Club</h1>
+    <h2> memberships for fit & club people </h2>
+    <nav>
+
+    </nav>
+    </header>
+    <main>
+    <p>Bienvenidx</p>
+    </main>
+    <footer>
+   <p> all rights reserved - developed by Paula Barroni </p> 
+    </footer>
+
+    </>
+    
+  
+    )
+}
+
+export default App
